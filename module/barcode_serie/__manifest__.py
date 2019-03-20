666a7987625ce8c96b7f4e56e5673b6fe360fe99
soto <victor.alonso@xmarts.com>
2019-03-20 15:15:28 -0600

    nuevos estilos
    se agregan nuevos campos

--- module/barcode_serie/__manifest__.py
+++ module/barcode_serie/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Barcode Series",
+    'name': "barcode_serie",
 
     'summary': """This module creates a bar code by means of a serial number.""",
 
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','stock'],
 
     # always loaded
     'data': [
--- module/barcode_serie/models/models.py
+++ module/barcode_serie/models/models.py
@@ -2,14 +2,7 @@
 
 from odoo import models, fields, api
 
-# class barcode_serie(models.Model):
-#     _name = 'barcode_serie.barcode_serie'
+class caja(models.Model):
+	_inherit = 'product.product'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+	caja=fields.Char(string='Caja de zapato')
