4e64a823a1e7c4b50cd65cf421e0860814438530
soto <victor.alonso@xmarts.com>
2019-04-26 10:31:03 -0500

    actualizacion
    se agrega el campo de color

--- module/barcode_serie/models/models.py
+++ module/barcode_serie/models/models.py
@@ -6,4 +6,14 @@ class caja(models.Model):
 	_inherit = 'product.product'
 
 	caja=fields.Char(string='Caja de zapato')
+	color=fields.Char(string='Color')
+
+class caja(models.Model):
+	_inherit = 'product.template'
+
+	caja=fields.Char(string='Caja de zapato')
+	color=fields.Char(string='Color')
+
+
+
 
